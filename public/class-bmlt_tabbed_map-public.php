4cb7770473701bd1c2e7a9b841b0c9e8cd5f3c42
Paul Nagle <paulnagle@gmail.com>
2018-11-11 20:48:24 +0000

    NO bootstrap or datatables

--- public/class-bmlt_tabbed_map-public.php
+++ public/class-bmlt_tabbed_map-public.php
@@ -22,8 +22,6 @@ class Bmlt_tabbed_map_Public
         wp_enqueue_style('marker_cluster_default', plugin_dir_url(__FILE__) . 'css/MarkerCluster.Default.css', array(), $this->version, 'all');
         wp_enqueue_style('fa_solid', 'https://use.fontawesome.com/releases/v5.4.1/css/solid.css', array(), $this->version, 'all');
         wp_enqueue_style('fa', 'https://use.fontawesome.com/releases/v5.4.1/css/fontawesome.css', array(), $this->version, 'all');
-        wp_enqueue_style('bootstrap_css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css', array(), $this->version, 'all');
-        wp_enqueue_style('data_table_css', plugin_dir_url(__FILE__) . 'css/dataTables.bootstrap4.min.css', array(), $this->version, 'all');
         wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/bmlt_tabbed_map-public.css', array(), $this->version, 'all');
     }
 
@@ -31,11 +29,6 @@ class Bmlt_tabbed_map_Public
     {
         // We need jquery 3.3.1
 
-        wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.3.1.slim.min.js', array(), $this->version, false);
-        wp_enqueue_script('popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array(), $this->version, false);
-        wp_enqueue_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js', array(), $this->version, false);
-        wp_enqueue_script('jquerydataTables', plugin_dir_url(__FILE__) . 'js/jquery.dataTables.min.js', array(), $this->version, false);
-        wp_enqueue_script('datatables', plugin_dir_url(__FILE__) . 'js/dataTables.bootstrap4.min.js', array(), $this->version, false);
         wp_enqueue_script('leaflet', plugin_dir_url(__FILE__) . 'js/leaflet.js', array(), $this->version, false);
         wp_enqueue_script('leafletlocate', plugin_dir_url(__FILE__) . 'js/L.Control.Locate.min.js', array(), $this->version, false);
         wp_enqueue_script('leafletmarker', plugin_dir_url(__FILE__) . 'js/leaflet.markercluster.js', array(), $this->version, false);
@@ -55,19 +48,20 @@ class Bmlt_tabbed_map_Public
         $output  = '
 
         <div class="bordered-tab-contents">
-          <nav class="nav nav-pills nav-fill flex-column flex-md-row" id="myTab" >
-            <a class="nav-item  nav-link" id="sunday-tab"    data-toggle="tab"  href="#sunday"    role="tab" >Sun <span id="sunday-badge"    class="badge  badge-primary"></span></a>
-            <a class="nav-item  nav-link" id="monday-tab"    data-toggle="tab"  href="#monday"    role="tab" >Mon <span id="monday-badge"    class="badge  badge-primary"></span></a>
-            <a class="nav-item  nav-link" id="tuesday-tab"   data-toggle="tab"  href="#tuesday"   role="tab" >Tue <span id="tuesday-badge"   class="badge  badge-primary"></span></a>
-            <a class="nav-item  nav-link" id="wednesday-tab" data-toggle="tab"  href="#wednesday" role="tab" >Wed <span id="wednesday-badge" class="badge  badge-primary"></span></a>
-            <a class="nav-item  nav-link" id="thursday-tab"  data-toggle="tab"  href="#thursday"  role="tab" >Thu <span id="thursday-badge"  class="badge  badge-primary"></span></a>
-            <a class="nav-item  nav-link" id="friday-tab"    data-toggle="tab"  href="#friday"    role="tab" >Fri <span id="friday-badge"    class="badge  badge-primary"></span></a>
-            <a class="nav-item  nav-link" id="saturday-tab"  data-toggle="tab"  href="#saturday"  role="tab" >Sat <span id="saturday-badge"  class="badge  badge-primary"></span></a>
-          </nav>
+
+        <ul id="tabs">
+          <li><a id="sundayTab"    >Sun <span id="sunday-badge"    class="badge"></span></a></li>
+          <li><a id="mondayTab"    >Mon <span id="monday-badge"    class="badge"></span></a></li>
+          <li><a id="tuesdayTab"   >Tue <span id="tuesday-badge"    class="badge"></span></a></li>
+          <li><a id="wednesdayTab" >Wed <span id="wednesday-badge"    class="badge"></span></a></li>
+          <li><a id="thursdayTab"  >Thu <span id="thursday-badge"    class="badge"></span></a></li>
+          <li><a id="fridayTab"    >Fri <span id="friday-badge"    class="badge"></span></a></li>
+          <li><a id="saturdayTab"  >Sat <span id="saturday-badge"    class="badge"></span></a></li>
+        </ul>
           <div id="map"> </div>
-          <div id="list_result"></div>
+          <div id="list_result">
           <script>
-            bmlt_tabbed_map_js.doIt();
+            bmltTabbedMapJS.doIt();
           </script>
         </div>';
         return $output;
