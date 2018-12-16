20871c3930b1884d2f5d8665903d4f0a3a88fd0d
Paul Nagle <paulnagle@gmail.com>
2018-12-16 08:56:24 +0000

    Change from generic PLUGIN_NAME_VERSION to BMLT_TABBED_MAP_PLUGIN_VERSION

--- bmlt_tabbed_map.php
+++ bmlt_tabbed_map.php
@@ -8,7 +8,6 @@
  * Description:       A plugin to display NA Meetings from the BMLT Tomato server on a map, tabbed by weekday.
  * Version:           1.0.0
  * Author:            Paul N
- * Author URI:        https://bmlt.app
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       bmlt_tabbed_map
@@ -19,7 +18,7 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-define( 'PLUGIN_NAME_VERSION', '1.0.0' );
+define( 'BMLT_TABBED_MAP_PLUGIN_VERSION', '1.0.0' );
 
 function activate_bmlt_tabbed_map() {
 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-bmlt_tabbed_map-activator.php';
